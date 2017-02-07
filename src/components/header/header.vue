2c7c68237411fa5a59a68db48547522679b2968d
zhangsong@optaim.com <zhangsong@optaim.com>
2017-02-07 18:04:28 +0800

    header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,11 +29,32 @@
 		<div class="background">
 			<img :src="seller.avatar" width="100%" height="100%">
 		</div>
-		<div v-show="detailShow" class="detail">
+		<div v-show="detailShow" class="detail"  name="fade">
 			<div class="detail-wrapper clearfix">
 				<div class="detail-main">
 					<h1 class="name">{{ seller.name }}</h1>
-					<star :size="48" :score="seller.score"></star>
+					<div class="star-wrapper">
+						<star :size="48" :score="seller.score"></star>
+					</div>
+					<div class="title">
+						<div class="line"></div>
+						<div class="text">优惠信息</div>
+						<div class="line"></div>
+					</div>
+					<ul v-if="seller.supports" class="supports">
+						<li class="support-item" v-for="(item,index) in seller.supports">
+							<span class="icon" :class="classMap[index]"></span>
+							<span class="text">{{ seller.supports[index].description }}</span>
+						</li>
+					</ul>
+					<div class="title">
+						<div class="line"></div>
+						<div class="text">商家公告</div>
+						<div class="line"></div>
+					</div>
+					<div class="bulletin">
+						<p class="content">{{ seller.bulletin }}</p>
+					</div>
 				</div>
 			</div>
 			<div class="detail-close" @click="hideDetail">x</div>
@@ -166,7 +187,13 @@
 		position: absolute;top: 0;left: 0;width: 100%;height: 100%; z-index: -1;filter: blur(10px);
 	}
 	.header .detail{
-		position: fixed;top:0;left:0;z-index: 100;width: 100%;height: 100%;overflow: auto;background:rgba(7,17,27,0.8);
+		position: fixed;top:0;left:0;z-index: 100;width: 100%;height: 100%;overflow: auto;background:rgba(7,17,27,0.8);backdrop-filter:blur(10px);
+	}
+	.header .detail.fade-enter-active,.header .detail.fade-leave-active {
+		transition: opacity .5s;
+	}
+	.header .detail.fade-enter,.header .detail.fade-leave-active {
+		opacity: 0;
 	}
 	.clearfix{
 		display: inline-block;
@@ -184,7 +211,56 @@
 	.detail .detail-wrapper .detail-main .name{
 		line-height: 16px;text-align: center;font-size: 16px;font-weight: 700;
 	}
+	.detail .detail-wrapper .detail-main .star-wrapper{
+		margin-top: 18px;padding: 2px 0;text-align: center;
+	}
+	.detail .detail-wrapper .detail-main .title{
+		width: 80%;margin:30px auto;display: flex; 
+	}
+	.detail .detail-wrapper .detail-main .title .line{
+		flex: 1;position: relative;top: -6px;border-bottom: 1px solid rgba(255,255,255,0.2);
+	}
+	.detail .detail-wrapper .detail-main .title .text{
+		padding: 0 5px;font-size: 14px;font-weight: 700;
+	}
+	.detail .detail-wrapper .detail-main .supports{
+		width: 80%;margin:0 auto; 
+	}
+	.detail .detail-wrapper .detail-main .supports .support-item{
+		padding: 0 12px;margin-bottom: 12px;font-size:0;
+	}
+	.detail .detail-wrapper .detail-main .supports .support-item:last-child{
+		margin-bottom: 0;
+	}
+	.detail .detail-wrapper .detail-main .supports .support-item .icon{
+		display: inline-block;width: 16px;height: 16px;vertical-align: top;margin-right: 6px;
+	}
+	.detail .detail-wrapper .detail-main .supports .support-item .icon.decrease{
+		background:url(decrease_2@2x.png) no-repeat; background-size: 16px 16px; 
+	}
+	.detail .detail-wrapper .detail-main .supports .support-item .icon.discount{
+		background:url(discount_2@2x.png) no-repeat; background-size: 16px 16px; 
+	}
+	.detail .detail-wrapper .detail-main .supports .support-item .icon.gurantee{
+		background:url(guarantee_2@2x.png) no-repeat; background-size: 16px 16px; 
+	}
+	.detail .detail-wrapper .detail-main .supports .support-item .icon.invoice{
+		background:url(invoice_2@2x.png) no-repeat; background-size: 16px 16px; 
+	}
+	.detail .detail-wrapper .detail-main .supports .support-item .icon.special{
+		background:url(special_2@2x.png) no-repeat; background-size: 16px 16px; 
+	}
+	.detail .detail-wrapper .detail-main .supports .support-item .text{
+		line-height: 16px;font-size: 12px;
+	}
+	.detail .detail-wrapper .detail-main .bulletin{
+		width: 80%;margin: 0 auto;
+	}
+	.detail .detail-wrapper .detail-main .bulletin .content{
+		padding: 0 12px;line-height: 24px;font-size: 12px;
+	}
 	.detail .detail-close{
 		position: relative;width: 32px;height: 32px;margin:-64px auto;clear: both;font-size: 32px; 
 	}
+
 </style>
--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="star" :class="starType">
-		<span v-for="itemClass in itemClasses" :class="itemClass" class="star-item"></span>
+		<span v-for="itemClass in itemClasses" :class="itemClass" class="star-item" track-by="$index"></span>
 	</div>
 </template>
 
@@ -22,7 +22,7 @@
 	starType() {
 	return 'star-' + this.size;
 	},
-	itemClass() {
+	itemClasses() {
 	let result = [];
 	let score = Math.floor(this.score * 2) / 2;
 	let hasDecimal = score % 1 !== 0;
@@ -48,49 +48,49 @@
 	.star .star-item{
 		display: inline-block;background-repeat: no-repeat;
 	}
-	.star .star-48 .star-item{
+	.star-48 .star-item{
 		width: 20px;height: 20px;margin-right: 22px;
 	}
-	.star .star-48 .star-item .on{
+	.star-48 .star-item.on{
 		background: url('star48_on@2x.png') no-repeat;background-size: 20px 22px;
 	}
-	.star .star-48 .star-item .half{
+	.star-48 .star-item.half{
 		background: url('star48_half@2x.png') no-repeat;background-size: 20px 22px;
 	}
-	.star .star-48 .star-item .off{
+	.star-48 .star-item.off{
 		background: url('star48_off@2x.png') no-repeat;background-size: 20px 22px;
 	}
-	.star .star-48 .star-item:last-child{
+	.star-48 .star-item:last-child{
 		margin-right: 0;
 	}
-	.star .star-36 .star-item{
+	.star-36 .star-item{
 		width: 15px;height: 15px;margin-right: 6px;
 	}
-	.star .star-36 .star-item .on{
+	.star-36 .star-item.on{
 		background: url('star36_on@2x.png') no-repeat;background-size: 15px 15px;
 	}
-	.star .star-36 .star-item .half{
+	.star-36 .star-item.half{
 		background: url('star36_half@2x.png') no-repeat;background-size: 15px 15px;
 	}
-	.star .star-36 .star-item .off{
+	.star-36 .star-item.off{
 		background: url('star36_off@2x.png') no-repeat;background-size: 15px 15px;
 	}
-	.star .star-36 .star-item:last-child{
+	.star-36 .star-item:last-child{
 		margin-right: 0;
 	}
-	.star .star-24 .star-item{
+	.star-24 .star-item{
 		width: 10px;height: 10px;margin-right: 3px;
 	}
-	.star .star-24 .star-item .on{
+	.star-24 .star-item.on{
 		background: url('star24_on@2x.png') no-repeat;background-size: 10px 10px;
 	}
-	.star .star-24 .star-item .half{
+	.star-24 .star-item.half{
 		background: url('star24_half@2x.png') no-repeat;background-size: 10px 10px;
 	}
-	.star .star-24 .star-item .off{
+	.star-24 .star-item.off{
 		background: url('star24_off@2x.png') no-repeat;background-size: 10px 10px;
 	}
-	.star .star-24 .star-item:last-child{
+	.star-24 .star-item:last-child{
 		margin-right: 0;
 	}
 </style>
